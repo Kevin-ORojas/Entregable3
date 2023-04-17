9c88ae2f05d022de27a0f72f6da4d38928ba98c5
NozoDev <kevinroj326@gmail.com>
2023-04-16 21:02:16 -0500

    Cambio tamaño barra de busqueda! responsiv

--- src/App.jsx
+++ src/App.jsx
@@ -46,23 +46,25 @@ function App() {
       </div>
 
       <form className="" onSubmit={handleSubmit}>
-        <div className="flex justify-center py-8">
+        <div className="flex justify-center py-2 mt-[-120px]">
           <input
-            className="border-green-400 bg-black text-[#938686] font-bold px-12 py-2  border-[2px]"
+            className="border-green-400  bg-black text-[#938686] font-bold sm:px-10 px-1 py-2  border-[2px]"
             id="locationId"
             placeholder="Type a location id ..."
             type="text"
           />
 
-          <button className="text-white border-green-500 bg-green-800/50 border-[2px] py-[8px] px-10 ">
+          <button className="text-white border-green-500 bg-green-800/50 border-[2px] py-[8px] px-1 sm:px-10 md:px-4  ">
             Search
-            <i className="absolute px-4 -translate-y-[-5px] mx-auto  bx bx-search-alt-2 "></i>
+            <i className=" px-1  -translate-y-[-5px] mx-auto  bx bx-search-alt-2 "></i>
           </button>
         </div>
 
-        <h2 className="text-green-400 text-center font-bold text-xl">
-          Welcome to the crazy universe!
-        </h2>
+        <div className="mt-16">
+          <h2 className="text-green-400 text-center font-bold text-xl">
+            Welcome to the crazy universe!
+          </h2>
+        </div>
       </form>
       <Location location={location} />
       <ResidentList location={location} />
